f515d7625f2b211ad153cb4444330317915eb3dd
Amos L King <amos.l.king@gmail.com>
2017-03-24 11:05:41 -0500

    Allow xml to not be order dependent
    Our old version of testing required all the xml elements to be in a
    specific order, and that is not required by the spec. I pulled in an
    older xml equivalence gem that hasn't been updated in a while, but seems
    to do the job really well. It has an option for ignoring attributes
    which we want for our ids.
    
    Amos King @adkron <amos@binarynoggin.com>

--- spec/package_spec.rb
+++ spec/package_spec.rb
@@ -1,7 +1,17 @@
 require "spec_helper"
 require "support/matchers/have_part_at"
+require "equivalent-xml"
 
 describe OpenXml::Pptx::Package do
+  def self.pptx(pptx_name)
+    pathname = Pathname("spec/fixtures/#{pptx_name}.pptx")
+    OpenXml::Pptx::Package.open(pathname.realpath)
+  end
+
+  def pptx(pptx_name)
+    self.class.pptx(pptx_name)
+  end
+
   context "when starting a new package" do
     subject { described_class.new }
 
@@ -13,11 +23,9 @@ describe OpenXml::Pptx::Package do
       expect(entries_of(subject)).to contain_exactly(*entries_of(pptx("minimal")))
     end
 
-    specify do
-      aggregate_failures do
-        pptx("minimal").parts.each do |part_path, expected_part|
-          is_expected.to have_part_at(part_path).with_content(expected_part.content)
-        end
+    pptx("minimal").parts.each do |part_path, expected_part|
+      specify do
+        expect(content_of(subject, part_path)).to be_equivalent_to(expected_part.content).ignoring_attr_values("Id")
       end
     end
   end
@@ -26,8 +34,7 @@ describe OpenXml::Pptx::Package do
     package.parts.keys
   end
 
-  def pptx(pptx_name)
-    pathname = Pathname("spec/fixtures/#{pptx_name}.pptx")
-    OpenXml::Pptx::Package.open(pathname.realpath)
+  def content_of(package, part_path)
+    package.get_part(part_path).content
   end
 end
